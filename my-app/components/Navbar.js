160284169c957bd5262c377487e407c035b797ef
unknown <silverzone42@gmail.com>
2022-08-13 21:43:59 +0530

    app

--- my-app/components/Navbar.js
+++ my-app/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar(){
                 <li><Link href="/"><a>Home</a></Link></li>
                 <li><Link href="/about"><a>About</a></Link></li>
                 <li><Link href="/"><a>Projects</a></Link></li>
-                <li><ConnectButton/></li>
+               <li> <ConnectButton chainStatus="none" showBalance={false} /></li> 
             </ul>
         </div>        
         {/* <ConnectButton/> */}
