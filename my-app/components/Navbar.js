5d08a833f82f6b10082b983f9b9b2949c66f5a00
karthik g.k <kar221694@gmail.com>
2022-08-13 14:21:59 -0400

    fundpage

--- my-app/components/Navbar.js
+++ my-app/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar(props){
                 <li><Link href="/"><a>Home</a></Link></li>
                 <li><Link href="/about"><a>About</a></Link></li>
                 <li><Link href="/projects"><a>Projects</a></Link></li>
-                {(MANAGER == props.address)?<li><Link href="/form"><a>create Request</a></Link></li>:<li><Link href="/"><a>Fund</a></Link></li>}
+                {(MANAGER == props.address)?<li><Link href="/form"><a>create Request</a></Link></li>:<li><Link href="/Fund"><a>Fund</a></Link></li>}
                <li> <ConnectButton chainStatus="none" showBalance={false} /></li> 
             </ul>
         </div>        
